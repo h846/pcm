467b48181c185b27b0c4c029f663d0a332e7e0e1
h846 <h846@outlook.com>
2020-12-14 17:11:51 +0900

    create template

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,5 @@
 <template>
   <v-app>
-    <v-navigation-drawer app v-model="drawer"
-      >Navigation Lists</v-navigation-drawer
-    >
-    <v-app-bar color="primary" dark app>
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-toolbar-title>PC Managemet System</v-toolbar-title>
-    </v-app-bar>
     <v-main>
       <router-view />
     </v-main>
@@ -16,11 +9,7 @@
 <script>
 export default {
   name: "App",
-
-  components: {},
-
-  data: () => ({
-    drawer: null,
-  }),
 };
 </script>
+<style scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,96 @@
 <template>
-  <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-card elevation="5" max-width="90%" class="mx-auto my-3">
+    <v-toolbar>
+      PC Management System
+      <v-spacer></v-spacer>
+      <v-text-field
+        v-model="search"
+        append-icon="mdi-magnify"
+        label="Search"
+        single-line
+        hide-details
+      ></v-text-field>
+
+      <v-dialog v-model="dialogDetail"> aa</v-dialog>
+
+      <v-dialog v-model="dialogDelete" max-width="500px">
+        <v-card>
+          <v-card-title>削除してよろしいですか？</v-card-title>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="blue darken-1" text @click="dialogDelete = false"
+              >キャンセル</v-btn
+            >
+            <v-btn color="blue darken-1" text @click="deleteItem">削除</v-btn>
+            <v-spacer></v-spacer>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-toolbar>
+
+    <v-data-table
+      :headers="headers"
+      :items="items"
+      :items-per-page="5"
+      :search="search"
+      sort-by="Computer_Name"
+      class="elevation-1"
+    >
+      <template v-slot:item.LoginTime="{ item }"></template>
+
+      <template v-slot:item.actions="{ item }">
+        <v-icon small class="mr-2" @click="showDetail(item)">mdi-pencil</v-icon>
+        <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
+      </template>
+    </v-data-table>
+  </v-card>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      test: "",
+      dialogDetail: false,
+      dialogDelete: false,
+      search: "",
+      headers: [
+        { text: "PC Name", value: "Computer_Name" },
+        { text: "User Name", value: "User_Name" },
+        { text: "Model", value: "Model" },
+        { text: "CPU", value: "CPU" },
+        { text: "RAM", value: "RAM" },
+        { text: "Type", value: "Type" },
+        { text: "OS", value: "OS" },
+        { text: "Office", value: "Office" },
+        { text: "Last Login", value: "LoginTime" },
+        { text: "IP", value: "IP_Address" },
+        { text: "Sirial#", value: "PC_Sirial" },
+        { text: "Remarks", value: "Remarks" },
+        { text: "Purchase Date", value: "Purchase_Date" },
+        { text: "Disposial Date", value: "Disposial_Date" },
+        { text: "Actions", value: "actions", sortable: false },
+      ],
+      items: [],
+    };
+  },
+  created() {
+    axios
+      .get("http://lejnet/API/accdb?db=ISNet/PCM/PCM.accdb&table=PCList")
+      .then((res) => {
+        this.items = res.data;
+      });
+  },
+  methods: {
+    showDetail: function (item) {
+      this.dialogDetail = true;
+    },
+    deleteItem: function (item) {
+      this.dialogDelete = true;
+    },
   },
 };
 </script>
