6467856692062bf6d64ba14f649b196275c8fd3f
h846 <h846@outlook.com>
2020-12-15 22:18:10 +0900

    add show detail func & delete record func

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-card elevation="5" max-width="90%" class="mx-auto my-3">
+  <v-card max-width="90%" class="mx-auto my-3">
     <v-toolbar>
       PC Management System
       <v-spacer></v-spacer>
@@ -10,39 +10,69 @@
         single-line
         hide-details
       ></v-text-field>
-
-      <v-dialog v-model="dialogDetail"> aa</v-dialog>
-
-      <v-dialog v-model="dialogDelete" max-width="500px">
-        <v-card>
-          <v-card-title>削除してよろしいですか？</v-card-title>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn color="blue darken-1" text @click="dialogDelete = false"
-              >キャンセル</v-btn
-            >
-            <v-btn color="blue darken-1" text @click="deleteItem">削除</v-btn>
-            <v-spacer></v-spacer>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
     </v-toolbar>
 
     <v-data-table
       :headers="headers"
       :items="items"
-      :items-per-page="5"
+      :items-per-page="10"
       :search="search"
       sort-by="Computer_Name"
       class="elevation-1"
     >
-      <template v-slot:item.LoginTime="{ item }"></template>
-
       <template v-slot:item.actions="{ item }">
         <v-icon small class="mr-2" @click="showDetail(item)">mdi-pencil</v-icon>
         <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
       </template>
+
+      <!-- <template v-slot:item.LoginTime="{ item }"> </template> -->
     </v-data-table>
+    <!-- Deatail Dialog -->
+    <v-dialog v-model="dialogDetail" max-width="500px">
+      <v-card>
+        <v-card-title><span class="headline">PC Detail</span></v-card-title>
+
+        <v-card-text>
+          <v-container>
+            <v-text-field
+              v-for="(val, key) in item"
+              :key="val"
+              :label="key"
+              :value="val"
+            >
+            </v-text-field>
+          </v-container>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" text @click="dialogDetail = flase">
+            Close
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <!-- Delete Dialog -->
+    <v-dialog v-model="dialogDelete" max-width="500px">
+      <v-card>
+        <v-card-title>削除してよろしいですか？</v-card-title>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" text @click="dialogDelete = false"
+            >キャンセル</v-btn
+          >
+          <v-btn color="blue darken-1" text @click="delRecord(item)"
+            >削除</v-btn
+          >
+          <v-spacer></v-spacer>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+    <!--loading-->
+    <v-overlay :value="overlay">
+      <v-progress-circular indeterminate size="64"></v-progress-circular>
+    </v-overlay>
   </v-card>
 </template>
 
@@ -56,22 +86,25 @@ export default {
       test: "",
       dialogDetail: false,
       dialogDelete: false,
+      overlay: false,
+      item: {},
+      loginTime: "",
       search: "",
       headers: [
         { text: "PC Name", value: "Computer_Name" },
         { text: "User Name", value: "User_Name" },
         { text: "Model", value: "Model" },
-        { text: "CPU", value: "CPU" },
-        { text: "RAM", value: "RAM" },
-        { text: "Type", value: "Type" },
+        //{ text: "CPU", value: "CPU" },
+        //{ text: "RAM", value: "RAM" },
+        //{ text: "Type", value: "Type" },
         { text: "OS", value: "OS" },
-        { text: "Office", value: "Office" },
+        //{ text: "Office", value: "Office" },
         { text: "Last Login", value: "LoginTime" },
-        { text: "IP", value: "IP_Address" },
-        { text: "Sirial#", value: "PC_Sirial" },
-        { text: "Remarks", value: "Remarks" },
-        { text: "Purchase Date", value: "Purchase_Date" },
-        { text: "Disposial Date", value: "Disposial_Date" },
+        //{ text: "IP", value: "IP_Address" },
+        //{ text: "Sirial#", value: "PC_Sirial" },
+        //{ text: "Remarks", value: "Remarks" },
+        //{ text: "Purchase Date", value: "Purchase_Date" },
+        //{ text: "Disposial Date", value: "Disposial_Date" },
         { text: "Actions", value: "actions", sortable: false },
       ],
       items: [],
@@ -87,9 +120,30 @@ export default {
   methods: {
     showDetail: function (item) {
       this.dialogDetail = true;
+      this.item = Object.assign({}, item);
     },
     deleteItem: function (item) {
       this.dialogDelete = true;
+    },
+    delRecord: function () {
+      let pcName = this.item["Computer_Name"];
+      axios
+        .post("http://lejnet/API/accdb", {
+          db: "ISNet/PCM/PCM.accdb",
+          sql: `DELETE FROM PCList WHERE Computer_Name = '${pcName}'`,
+        })
+        .then((res) => {
+          console.log(res);
+          this.overlay = true;
+          //再取得
+          axios
+            .get("http://lejnet/API/accdb?db=ISNet/PCM/PCM.accdb&table=PCList")
+            .then((res) => {
+              this.items = res.data;
+            });
+          this.dialogDelete = false;
+          this.overlay = false;
+        });
     },
   },
 };
